15799395054e7a40cd28e7dfe514abb37eaddbd9
FRANCIS ROSARIO <CEO@INGEN.COM.PH>
2025-08-07 18:43:22 +0800

    update: use cloud

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://isproj2.ingen.com.ph';
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'https://isproj2.ingen.com.ph';
+// const BASE_URL = 'http://localhost:3001';
 const LS_KEY = 'jwt_token';
 
 function getToken() {
